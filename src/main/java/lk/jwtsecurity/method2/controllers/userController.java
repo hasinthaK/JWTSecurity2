0669b64c4e520a1f9363ed6e2ef1567638f7d6fb
hasinthaK <hasiroxsola@gmail.com>
2019-11-17 23:22:45 +0530

    Initial commit

--- src/main/java/lk/jwtsecurity/method2/controllers/userController.java
+++ src/main/java/lk/jwtsecurity/method2/controllers/userController.java
@@ -53,3 +53,9 @@ public class userController {
     }
 
 }
+
+//    Spring boot security JWT implementation method 2.
+//
+//        http://localhost:8080/login with credentials to aquire a JWT.
+//
+//        http://localhost:8080/register with credentials to sign up.
